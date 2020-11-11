bb81f3763ef8da36d86564087eb3caf23166396d
Shayan Shariff <ss05582@st.habib.edu.pk>
2020-11-12 01:36:33 +0500

    added operate func

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b){
 function divide(a, b){
     return a/b;
 } 
+
+function operate(operator, a, b){
+    let ans;
+    switch(operator){
+        case "+":
+            ans = add(a, b);
+            break;
+        case "-":
+            ans = subtract(a,b);
+            break;
+        case "*":
+            ans = multiply(a,b);
+            break;
+        case "/":
+            ans = divide(a,b);
+            break;
+    }
+    return ans;
+}
